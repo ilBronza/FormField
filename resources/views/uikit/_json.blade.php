8e2aa3787e23261c0a838f1e3a0b6888282c3906
Davide Bronzini <davide_bronzini@hotmail.com>
2020-09-17 13:18:15 +0200

    json field

---
+++ resources/views/uikit/_json.blade.php
@@ -0,0 +1,60 @@
+@include('formfield::uikit.formRowHeader')
+
+<div class="valuescontainer">
+	<div
+		uk-grid
+		>
+		<div uk-grid class="uk-child-width-1-2 uk-clearfix uk-form-stacked uk-width-expand">
+			@foreach($field->innerFields as $innerField)
+			<label>{{ __('fields.' . $innerField->subName) }}</label>
+			@endforeach			
+		</div>
+		<div class="uk-width-auto">
+			<button type="button" data-id="{{ $overrideId ?? ($field->getId() . (isset($fieldIndex)? ('-' . $fieldIndex) : '')) }}" class="addjsonfake" uk-icon="plus"></button>
+		</div>
+		
+	</div>
+
+	@foreach($field->getFormOldValue() as $key => $value)
+
+		<div
+			id="{{ $overrideId ?? ($field->getId() . (isset($fieldIndex)? ('-' . $fieldIndex) : '')) }}"
+			class="jsonvalues"
+			uk-grid
+			>
+			<div uk-grid class="uk-child-width-1-2 uk-clearfix uk-form-stacked uk-width-expand">
+				@foreach($field->getInnerFieldsByKeyValue($key, $value) as $innerField)
+					{!! $innerField->setValue($value[$innerField->subName])->render() !!}
+				@endforeach			
+			</div>
+			<div class="uk-width-auto">
+				<span uk-icon="minus" class="jsonvaluesremover"></span>
+			</div>
+			
+		</div>
+
+	@endforeach
+</div>
+
+
+<div class="valuestemplate">
+	<div
+		id="{{ $overrideId ?? ($field->getId() . (isset($fieldIndex)? ('-' . $fieldIndex) : '')) }}"
+		class="jsonvalues fakejsonvalues"
+		uk-grid
+		>
+		<div uk-grid class="uk-child-width-1-2 uk-clearfix uk-form-stacked uk-width-expand">
+			@foreach($field->innerFields as $innerField)
+				{!! $innerField->setValue(null)->render() !!}
+			@endforeach			
+		</div>
+		<div class="uk-width-auto">
+			<span uk-icon="minus" class="jsonvaluesremover"></span>
+		</div>
+		
+	</div>
+</div>
+
+<button type="button" data-id="{{ $overrideId ?? ($field->getId() . (isset($fieldIndex)? ('-' . $fieldIndex) : '')) }}" class="addjson" uk-icon="plus">{{ __('fields.addInstance') }}</button>
+
+@include('formfield::uikit.formRowFooter')
---
+++ src/Fields/JsonFormField.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace ilBronza\FormField\Fields;
+
+use Illuminate\Support\Str;
+use ilBronza\FormField\Fields\FormFieldInterface;
+use ilBronza\FormField\FormField;
+
+class JsonFormField extends FormField implements FormFieldInterface
+{
+	// use SingleValueFormFieldTrait;
+	// use ListValueFormFieldTrait;
+	// use RelationshipFormFieldTrait;
+
+	// public $htmlClasses = [
+	// 		'uk-input'
+	// 	];
+
+	public $innerFields;
+	public $showLabels = false;
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->innerFields = collect();
+	}
+
+	public function getValue()
+	{
+		if(isset($this->value))
+			return $this->value;
+
+		if($this->model)
+			return $this->getModelValueByName($this->model, $this->name);
+
+		if(($this->form)&&($this->form->model))
+			return $this->getModelValueByName($this->form->model, $this->name);
+
+		if(! empty($this->default))
+			return $this->default;
+
+		return null;
+		// throw new \Exception('Nessun model da dove prendere il dato');
+	}
+
+	public function getFormOldValue()
+	{
+		return old(
+			$this->getFormOldName(),
+			$this->getValue()
+		);
+	}
+
+	private function manageLabel(FormField $formField) : FormField
+	{
+		if($this->showLabels == false)
+			$formField->setLabel(false);
+
+		return $formField;
+	}
+
+	private function manageForm(FormField $formField) : FormField
+	{
+		if(! $formField->form)
+			$formField->form = $this->form;
+
+		return $formField;
+	}
+
+	private function manageName(FormField $formField) : FormField
+	{
+		$formField->subName = $formField->name;
+		$formField->name = $this->name . '[counter][' . $formField->name . ']';
+
+		return $formField;
+	}
+
+	private function managePlaceholder(FormField $formField) : FormField
+	{
+		if(! $formField->placeholder)
+			$formField->placeholder = __('fields.' . Str::slug($formField->getName(), '_'));
+
+		return $formField;
+	}
+
+	private function removeId(FormField $formField) : FormField
+	{
+		$formField->setId(false);
+
+		return $formField;
+	}
+
+	public function getInnerFieldsByKeyValue(string $key, array $value)
+	{
+		$result = collect();
+
+		foreach($this->innerFields as $innerField)
+		{
+			$innerField->name = str_replace("counter", $key, $innerField->name);
+			$innerField->setValue($value[$innerField->subName]);
+
+			$result->push($innerField);
+		}
+
+		return $result;
+	}
+
+	public function addFormField(FormField $formField) : FormField
+	{
+		$formField = $this->manageLabel($formField);
+		$formField = $this->manageForm($formField);
+		$formField = $this->managePlaceholder($formField);
+		$formField = $this->manageName($formField);
+		$formField = $this->removeId($formField);
+
+		$this->innerFields->push($formField);
+
+		return $formField;
+	}
+}
