d6beb304577b20fab64a268b029a8f5ccba36029
Davide Bronzini <davide_bronzini@hotmail.com>
2020-10-06 12:20:03 +0200

    password

---
+++ resources/views/uikit/_password.blade.php
@@ -0,0 +1,13 @@
+@include('formfield::uikit.formRowHeader')
+
+<input
+	@include('formfield::__data')
+	@include('formfield::__attributes')
+
+	class="uk-input" 
+	type="password"
+	value=""
+
+	/>
+
+@include('formfield::uikit.formRowFooter')
